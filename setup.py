756f1bbedc36d603ee1d97e18d771c17cf8ffb01
Vivekanand <vivek@vidooly.com>
2018-10-29 15:55:01 +0530

    Sys Util

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+setup(
+  name = 'sysutil',
+  packages = ['sysutil'], # this must be the same as the name above
+  version = '0.2',
+  description = 'A lib for common methods used in project',
+  author = 'Vivekanand Pandey',
+  author_email = 'vivekanandpandey98@gmail.com',
+  url = 'https://github.com/vmartian/sysutil.git',
+  download_url = 'https://github.com/vmartian/sysutil/archive/master.zip',
+  keywords = ['common','system','library'], 
+  classifiers = [],
+)

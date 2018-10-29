08735263f187675e455567ac901aab2dd1edc715
Vivekanand <vivek@vidooly.com>
2018-10-29 15:12:13 +0530

    Added Base Path

--- base_path.py
+++ base_path.py
@@ -2,14 +2,14 @@ import sys
 import os
 import inspect
 import pathlib
-def get_base_path():
+def get_base_path(base_file='constants.py'):
     file_path = os.getcwd()
     path_arr = file_path.split('/')
     base_path = '/'
     for path in path_arr:
         if path != '':
            base_path += "{}/".format(path)
-           file = pathlib.Path(base_path+"/constants.py")
+           file = pathlib.Path(base_path+"/"+base_file)
            if file.exists():
               return base_path
     return 
